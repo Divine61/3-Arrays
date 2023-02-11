7097270b76e7c33be2b4bc8808edda63643044e5
Kirill Davidenko <stafort777@gmail.com>
2023-02-12 01:16:08 +0300

    1 task draft

--- task.js
+++ task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+    return ((arr1.length === arr2.length) && arr1.every((element, index) => element === arr2[index]));
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = users.filter(user => user.gender === gender).map(old => old.age).reduce((acc, old, index, arr) => {
+    acc += old;
+    if (index === arr.length - 1) {
+        return acc / arr.length;
+    }
+    return acc;
+  }, 0);
+  return result;
 }
